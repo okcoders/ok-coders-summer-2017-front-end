404aa63b3ba5e476e4dc8116a7cffa9f3b1354c7
Zachary Mays <zach@oseberg.io>
2016-04-07 17:56:19 -0500

    examples for grid and 2 dimension arrays

---
+++ exercises/week-3/collections.js
@@ -0,0 +1,16 @@
+var ticTacToe = [
+  ['x', 'o', 'o'],
+  ['x', 'o', 'x'],
+  ['x', 'x', 'o']
+]
+
+// log the top left corner
+console.log(ticTacToe[0][0])
+
+// log the second row, first column
+console.log(ticTacToe[1][0])
+
+// log the third row, third column
+console.log(ticTacToe[2][2])
+
+console.log(ticTacToe)
