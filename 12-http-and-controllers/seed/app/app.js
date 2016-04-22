4710aac3cbf3712b8a485283803dbfbee034d470
Zachary Mays <zach@oseberg.io>
2016-04-21 20:03:12 -0500

    in class stuff

--- 12-http-and-controllers/seed/app/app.js
+++ 12-http-and-controllers/seed/app/app.js
@@ -6,7 +6,18 @@ angular.module('ourSweetApp')
 
 function ourFirstController() {
   var vm = this
+  vm.show = true
 
   vm.ourFirstVariable = 'Wooooooooo!!!!!!'
+  vm.people = [
+    {first: 'zach', last: 'mays', age: 26},
+    {first: 'doug', last: 'fresh', age: 34},
+    {first: 'prince', last: 'prince', age: 57.4}
+  ]
+
+  vm.respondToClick = function() {
+    vm.show = false
+    console.log("Holy poop! This is awesome")
+  }
 }
 
